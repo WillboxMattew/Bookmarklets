73cf754cffb33cfccc194dbf27070f3b0c6020da
W S <137005470+WillboxMattew@users.noreply.github.com>
2023-06-22 23:36:21 +0000

    Create MediumMob.js

---
+++ TabMob/MediumMob.js
@@ -0,0 +1,4 @@
+var url=prompt("url");
+var i=0;
+var handle=setInterval(()=>{ window.open(url); i++; } , parseInt(prompt("interval (ms)")));
+document.getElementsByTagName("body")[0].onclick=(()=>{ clearInterval(handle); });
